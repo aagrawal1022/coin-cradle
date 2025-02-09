51dfce20385139ce00a7ef3c7e6e9f0d39075b91
aagrawal1022 <76031088+aagrawal1022@users.noreply.github.com>
2025-02-10 00:58:23 +0530

    Issue-1 Java Skleteon Added

---
+++ backend/src/main/java/com/coincradle/CoinCradleDailyExpenseManagementApplication.java
@@ -0,0 +1,12 @@
+package com.coincradle.coin_cradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinCradleDailyExpenseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinCradleDailyExpenseManagementApplication.class, args);
+	}
+}
