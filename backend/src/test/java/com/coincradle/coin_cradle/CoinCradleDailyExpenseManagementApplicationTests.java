945e40f583e6bd09e39e9d6f5c913d14dac1af94
aagrawal1022 <76031088+aagrawal1022@users.noreply.github.com>
2025-02-10 00:58:23 +0530

    Issue-1 Java Skleteon Added

---
+++ backend/src/test/java/com/coincradle/coin_cradle/CoinCradleDailyExpenseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coincradle.coin_cradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinCradleDailyExpenseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
